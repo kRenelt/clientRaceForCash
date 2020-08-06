0293fd2f51bc105edbf948a05d31cd0bedc1eac3
Hugh Thornhill <hlowerthornhill@gmail.com>
2020-08-06 19:21:39 -0400

    Built out basic registration component, will need to add more styling and functionality

--- raceforcash/src/components/registration/registration.component.tsx
+++ raceforcash/src/components/registration/registration.component.tsx
@@ -1,9 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Form, Container, Row, Col, Button } from 'react-bootstrap';
 
 export const RegistrationComponent: React.FC = () => {
+
+    const [inputEmail, setInputEmail] = useState('');
+    const [inputPassword, setInputPassword] = useState('');
+    const [inputFirstName, setInputFirstName] = useState('');
+    const [inputLastName, setInputLastName] = useState('');
+
+    useEffect(() => {
+      }, []);
+
+    //   const loginCredentials = async () => {
+        const loginCredentials = () => {
+        const payload = {
+            email: inputEmail, 
+            password: inputPassword
+        };
+        // console.log(payload);
+        // try{
+        //     res = await loginRemote.checkLogin(payload);
+        //     await login();
+        // } catch(err) {
+        //     console.log(err);
+        // }
+    }
+
     return(
-        <div className='registration-main'>
-            Registration
-        </div>
+        <Container>
+            {/* <Row>
+                <Col> */}
+                    <Form>
+
+                        <Form.Label><h1>Registration</h1></Form.Label>
+
+                        <Form.Group controlId='formEmail'>
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control value={inputEmail} onChange={(e) => setInputEmail(e.target.value)} />
+                        </Form.Group>
+
+                        <Form.Group controlId='formPassword'>
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control value={inputPassword} onChange={(e) => setInputPassword(e.target.value)} />
+                        </Form.Group>
+
+                        <Form.Group controlId='formFirstName'>
+                            <Form.Label>First Name</Form.Label>
+                            <Form.Control value={inputFirstName} onChange={(e) => setInputFirstName(e.target.value)} />
+                        </Form.Group>
+
+                        <Form.Group controlId='formLastName'>
+                            <Form.Label>Last Name</Form.Label>
+                            <Form.Control value={inputLastName} onChange={(e) => setInputLastName(e.target.value)} />
+                        </Form.Group>
+
+                        <p>role</p>
+                    </Form>
+                    <Button type = "submit" onClick={() => loginCredentials()}>
+                        Register
+                    </Button>
+                {/* </Col>
+            </Row> */}
+        </Container>
     );
 }
