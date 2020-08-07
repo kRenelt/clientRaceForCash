a3659307a652dc184fdf03b69964af850666937f
Hugh Thornhill <hlowerthornhill@gmail.com>
2020-08-07 16:20:28 -0400

    Adjusted Race model to accpet an array for payout value

--- raceforcash/src/models/Race.ts
+++ raceforcash/src/models/Race.ts
@@ -1,6 +1,12 @@
+// May need to adjust array of model in future implementation
+interface Payout {
+    id: number;
+    amount: number;
+}
+
 export interface Race {
     id: number;
     name: string;
     startTime: Date;
-    payouts: number;
+    payout: Array<Payout>;
 }
