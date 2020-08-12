ec66d724121b11195f3b2401587cb4b8e1334019
Hugh Thornhill <hlowerthornhill@gmail.com>
2020-08-12 00:32:32 -0400

    Added methods to user remote

--- raceforcash/src/remotes/user.remote.ts
+++ raceforcash/src/remotes/user.remote.ts
@@ -1,2 +1,12 @@
 import { internalAxios } from './internal.axios'
 import { User } from '../models/User';
+
+export const checkCredentials = async (user: User) => {
+    const response = await internalAxios.post('/login', user);
+    return response;
+}
+
+export const getUserById = async (id: number) => {
+    const response = await internalAxios.get<User>(`/user/${id}`);
+    return response;
+}
